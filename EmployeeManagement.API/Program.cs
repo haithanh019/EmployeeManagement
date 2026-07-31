04fb2e067feb091f27705886692633e1d10c19fc
Hoàng Hải Thành <138685212+haithanh019@users.noreply.github.com>
2026-07-31 21:10:06 +0800

    refactor: remove OData, use standard REST API

--- EmployeeManagement.API/Program.cs
+++ EmployeeManagement.API/Program.cs
@@ -1,15 +1,11 @@
 using EmployeeManagement.API.Middlewares;
-using EmployeeManagement.BusinessObject.DTOs.EmployeeDTO;
 using EmployeeManagement.Repository;
 using EmployeeManagement.Repository.Implementations;
 using EmployeeManagement.Repository.Interfaces;
 using EmployeeManagement.Service.Implementations;
 using EmployeeManagement.Service.Interfaces;
 using EmployeeManagement.Service.Mappers;
-using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OData.Edm;
-using Microsoft.OData.ModelBuilder;
 using System.Text.Json.Serialization;
 
 namespace EmployeeManagement.API
@@ -20,36 +16,23 @@ namespace EmployeeManagement.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddJsonOptions(opt =>
+                {
+                    opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
+
             builder.Services.AddOpenApi();
+
             builder.Services.AddAutoMapper(cfg =>
             {
                 cfg.LicenseKey = builder.Configuration["AutoMapper:LicenseKey"];
                 cfg.AddProfile<MappingProfile>();
             });
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-            static IEdmModel GetEdmModel()
-            {
-                var odataBuilder = new ODataConventionModelBuilder();
-                odataBuilder.EntitySet<EmployeeDto>("Employees");
 
-                return odataBuilder.GetEdmModel();
-            }
-
-            builder.Services.AddControllers()
-            .AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-            })
-            .AddOData(options => options
-                .Select()
-                .Filter()
-                .OrderBy()
-                .Expand()
-                .Count()
-                .SetMaxTop(100)
-                .AddRouteComponents("Odata", GetEdmModel()));
             builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             builder.Services.AddScoped<IWorkLogRepository, WorkLogRepository>();
@@ -59,18 +42,17 @@ namespace EmployeeManagement.API
             builder.Services.AddScoped<ISalaryService, SalaryService>();
 
             var app = builder.Build();
+
             app.UseMiddleware<GlobalExceptionMiddleware>();
+
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
             app.MapControllers();
-
             app.Run();
         }
     }
